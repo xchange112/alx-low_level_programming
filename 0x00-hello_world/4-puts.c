1308aecc08a5131107dcd4124bbc00160b8c1999
xchange112 <solomonjeremiah001@gmail.com>
2022-10-13 15:23:11 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main(void)
-{	/**
-	 *prints to the output
-	 */
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+{
+	/**
+	 * main- Servers as the entry point for the program
+	 *
+	 * Return: By default returns zero if no errors otherwise returns non-zero value.
+	 * / 
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return(0);
 }

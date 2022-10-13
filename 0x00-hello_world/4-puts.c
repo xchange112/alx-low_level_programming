1717aee1ea67a6b75d2416b72f9826873c7fb2bc
xchange112 <solomonjeremiah001@gmail.com>
2022-10-13 15:29:11 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
-	/**
-	 * main- Servers as the entry point for the program
-	 *
-	 * Return: By default returns zero if no errors otherwise returns non-zero value.
-	 * / 
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return(0);
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
 }

0961c3c53d356701ac1299a80a2fe09da0ee764a
xchange112 <solomonjeremiah001@gmail.com>
2022-10-13 14:15:55 -0700

    Put hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#!/ bin / bash
 #include<stdio.h>
-int main(void) {
-	/*print the following word to the output*/
+int main(void)
+{	/**
+	 *prints to the output
+	 */
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }

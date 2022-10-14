ff7b665312fe790147778d4da14cd63e198a6b23
xchange112 <solomonjeremiah001@gmail.com>
2022-10-13 17:26:42 -0700

    Simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main _ Start point
+ * Return : End point and it's gonna give 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	return (1);

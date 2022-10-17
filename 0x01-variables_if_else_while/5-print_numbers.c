796cb4e17f187a83d04fa5023af7bc6f11e98950
xchange112 <solomonjeremiah001@gmail.com>
2022-10-17 01:51:08 -0700

    add numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - print aal base 10 digits i.e 0-9
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int i, base = 10;
+
+	for (i = 0; i < base; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}

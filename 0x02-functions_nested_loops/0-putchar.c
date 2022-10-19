8cf71d700cd05a3c2c374bba1576ec08450f09a9
xchange112 <solomonjeremiah001@gmail.com>
2022-10-18 21:11:38 -0700

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - print out '_putchar'
- * return: 0
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
 	return (0);
 }

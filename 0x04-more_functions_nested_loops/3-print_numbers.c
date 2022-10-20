48f94a14a9540e0e477f730f5b5dc8ed56066dcf
xchange112 <solomonjeremiah001@gmail.com>
2022-10-20 19:51:18 +0100

    multiply numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
+ * print_numbers - print numbers between 0 and 9
  * Return: return 0
  */
 void print_numbers(void)
 {
 	int i;
-	for (i = 0; i += 1; i++)
-		return (0);
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }

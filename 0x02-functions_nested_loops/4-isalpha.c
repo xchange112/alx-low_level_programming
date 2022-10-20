6e5386fd01e88109a9f2665c1d185aace4cbe838
xchange112 <solomonjeremiah001@gmail.com>
2022-10-19 19:28:29 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *_isalpha; RETURNS 1 if true else returns 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int _isalpha(int c);
+}
+int _isalpha(int c)
+{
+	/*This will check if the input is an alphabet*/
+	if (c == type(char))
+		return (1);
+	else
+		return (0);
+}

8443706a8ba1630ac81e21af86afcacaa2a14042
xchange112 <solomonjeremiah001@gmail.com>
2022-10-24 14:56:16 -0700

    new task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- *  * reset_to_98 - go into pointer address and change the value.
+ * reset_to_98 - go into pointer address and change the value.
  * @n: int type pointer
- *   
+ *
  */
 
 void reset_to_98(int *n)
